812c83ace5fef9cddaacbbd8e1343343d1b6acd5
Ma Phuc Minh Quan <maphucminhquan07@gmail.com>
2026-07-14 19:01:44 +0700

    Buoi2

--- Bai5/src/App.java
+++ Bai5/src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        int[] bang = {1,2,3,4,5,6,7,8,9};
+        int[] lan ={1,2,3,4,5,6,7,8,9};
+        for(int i: bang){
+            System.out.println("Bang cuu chuong "+i);
+            for(int y: lan){
+                System.out.println(i+" * "+y +" = "+i*y);
+            }
+            System.out.println();
+        }
+        int[] dong = {1, 2, 3, 4, 5};
+        for (int i : dong) {
+            String sao = "";
+            for (int j : dong) {
+                if (j <= i) {
+                    sao += "* ";
+                }
+            }
+            System.out.println(sao);
+        }
     }
 }
