153a1a9953bb3087759e5af355d2a4a57bb81192
Ma Phuc Minh Quan <maphucminhquan07@gmail.com>
2026-07-14 18:35:47 +0700

    Buoi2

--- Bai4/src/App.java
+++ Bai4/src/App.java
@@ -1,5 +1,25 @@
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so nguyen n: ");
+        n = sc.nextInt();
+        int tam = n ;
+        int so ;
+        int dao = 0;
+        while( tam != 0){
+            so = tam%10;
+            dao = dao * 10 + so ;
+            tam /=10;
+        }
+        System.out.println("so dao: "+ dao);
+        if( n == dao){
+            System.out.println("La so doi xung!");
+        }else{
+            System.out.println("Khong phai so doi xung!");
+        }
     }
 }
